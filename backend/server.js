0bc085f55b96724fe083f7a403de097874d3a599
Madhusri02 <madhusri0774@gmail.com>
2025-03-24 22:12:25 +0530

    f&b commit

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const exp = require('express')
+const cors = require('cors')
+const moong = require('mongoose')
+const userRouter = require('./routes/UserRoutes')
+
+const app = exp();
+
+app.use(exp.json());
+app.use(cors());
+
+
+app.use('/user' , userRouter)
+
+const port  = process.env.PORT || 8000 ;
+app.listen(port , ()=>{
+    console.log("running at server " + port)
+})
+
+
+async function conne(){
+    const check= await moong.connect('mongodb://localhost:27017/e-learning');
+    if(!check){
+        console.log("connection falied")
+
+    }
+    else{
+        console.log("connected to the DB")
+    }
+}
+
+
+
+
+
+conne();
+
+
+
+
+
+
+
