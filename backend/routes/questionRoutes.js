dbe1228be6316dfb268cc536bae9518ae49a019b
Harshi0916 <harshithasiva03@gmail.com>
2025-03-31 21:58:20 +0530

    updated code

--- backend/routes/questionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getEasyQuestions, getMediumQuestions, getHardQuestions } = require('../controllers/questionsController');
-
-router.get('/easy', getEasyQuestions);
-router.get('/medium', getMediumQuestions);
-router.get('/hard', getHardQuestions);
-
-module.exports = router;
