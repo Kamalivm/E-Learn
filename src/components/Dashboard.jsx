a1e76e103876545b4a9d1058af2c37c6552e58d7
Madhusri02 <madhusri0774@gmail.com>
2025-04-15 12:18:35 +0530

    swipe out

--- src/components/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-const Dashboard = () => {
-    return (
-        <div className="p-5">
-            <h2 className="text-2xl font-bold">Daily Activity</h2>
-            <p>Track your progress here.</p>
-        </div>
-    );
-};
-export default Dashboard;
