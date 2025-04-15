0fbc3049f41445cd9cc92583d0c31664eb9e32f8
Madhusri02 <madhusri0774@gmail.com>
2025-04-15 22:31:19 +0530

    update

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,14 +1,13 @@
 import express from 'express';
-import { signup, login } from '../controllers/userController.js'; // import login from controller
+import { signup, login } from '../controllers/userController.js'; 
 import authenticateToken from '../utils/MiddleWare.js';
 
 const router = express.Router();
 
-// Use actual login function
 router.post('/login', login);  
 
 router.post('/signup', signup);
-router.use(authenticateToken);  // protect all routes below this middleware
+router.use(authenticateToken);  
 router.get('/', (req, res) => res.send("User route is working"));
 
 export default router;
